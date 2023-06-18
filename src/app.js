f90159e4303b3d6a214ac27396ab118b355e736e
farangis-ha <farangis.hj@gmail.com>
2023-06-18 09:06:08 +0330

    make js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,10 @@
-let city = document.querySelector("enterCity");
-city = "hii";
-alert(city);
+let search = document.querySelector("#search");
+search.addEventListener(click, "showCity");
+function showCity() {
+  let searchValue = document.querySelector(
+    "form.d-flex searchPart.form-control"
+  );
+  let searhCity = searchValue.value;
+  let city = document.querySelector("#enter-city");
+  city.innerHTML = searhCity;
+}
