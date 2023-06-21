1eaab46daf81436a26e8f6baf3a05b5884b21c5e
farangis-ha <farangis.hj@gmail.com>
2023-06-21 11:21:34 +0330

    time+date

--- src/app.js
+++ src/app.js
@@ -1,10 +1,37 @@
-let search = document.querySelector("#search");
-search.addEventListener(click, "showCity");
-function showCity() {
-  let searchValue = document.querySelector(
-    "form.d-flex searchPart.form-control"
-  );
-  let searhCity = searchValue.value;
-  let city = document.querySelector("#enter-city");
-  city.innerHTML = searhCity;
+function setTime() {
+  let days = ["sun", "Mon", "Tue", "Wed", "Thu", "Fri", "sat"];
+  let day = days[now.getDay()];
+  let hour = now.getHours();
+  let min = now.getMinutes();
+
+  if (hour > 9) {
+    if (min > 9) {
+      today = `${day} ${hour}:${min}`;
+    } else {
+      today = `${day} ${hour}:0${min}`;
+    }
+  } else {
+    if (min > 9) {
+      today = `${day} 0${hour}:${min}`;
+    } else {
+      today = `${day} 0${hour}:0${min}`;
+    }
+  }
+  return today;
 }
+function showCity(event) {
+  event.preventDefault();
+  let searchValue = document.querySelector('[placeholder="Search"]');
+  let searchCity = searchValue.value;
+  let city = document.querySelector("#enter-city");
+  city.innerHTML = searchCity;
+}
+
+// Search City
+let search = document.querySelector("#search");
+search.addEventListener("click", showCity);
+//Date + Time
+let now = new Date();
+let dateIn = document.querySelector("#city-time");
+dateIn.innerHTML = setTime(now);
+// Wheather
